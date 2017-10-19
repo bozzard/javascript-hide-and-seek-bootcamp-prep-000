6bd016056daf79d26157a65d4f3a73730cb6a3da
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-19 13:25:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function getFirstSelector(selector) {
+/*function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
@@ -23,4 +23,38 @@ var lis = document.getElementById('grand-node').querySelectorAll('div');
 var l = (lis.length-1);
 return lis[l];
 
+}
+*/
+function getFirstSelector(s) {
+  return document.querySelector(s)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  console.log('node =' , node);
+  let nextNode = node.children[0]
+  console.log('nextNode =' , node);
+  while (nextNode) {
+    node = nextNode
+    console.log('node = nextNode ', node);
+    nextNode = node.children[0]
+  }
+
+  return node
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
 }
